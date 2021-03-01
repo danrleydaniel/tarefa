95fb0ec76cdbee47dd169c795103c84ee046b6f7
danrleydaniel <danrleydaniel21@gmail.com>
2021-03-01 11:54:31 -0300

    questão 3

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,11 @@ function HomePage() {
       <h2>Descrição</h2>
 
       <p>O Naruto pode ser um pouco duro às vezes. Talvez você não saiba disso, mas, o Naturo também cresceu sem pai. Na verdade, ele nunca conheceu nenhum de seus pais, e nunca teve nenhum amigo em nossa aldeia. Mesmo assim, eu nunca vi ele chorar, ficar zangado ou se dar por vencido, ele está sempre dispostos a melhorar, ele quer ser respeitado, é o sonho dele e o Naruto daria a vida por isso sem hesitar. Meu palpite é que ele parou de chorar e decidiu fazer alguma coisa a respeito.</p>
+
+
+      <Link href="/maria">
+        <a>Maria</a>
+      </Link>
     </div>
   )
 }
