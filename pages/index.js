0682f10f983a327dcc03600731658045660704ba
danrleydaniel <danrleydaniel21@gmail.com>
2021-03-01 11:51:26 -0300

    questão 2

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function HomePage() {
-  return <div>Welcome to Next.js!</div>
+  return (
+    <div>
+      <h1>Minha primeira página NextJS</h1>
+
+      <h2>Descrição</h2>
+
+      <p>O Naruto pode ser um pouco duro às vezes. Talvez você não saiba disso, mas, o Naturo também cresceu sem pai. Na verdade, ele nunca conheceu nenhum de seus pais, e nunca teve nenhum amigo em nossa aldeia. Mesmo assim, eu nunca vi ele chorar, ficar zangado ou se dar por vencido, ele está sempre dispostos a melhorar, ele quer ser respeitado, é o sonho dele e o Naruto daria a vida por isso sem hesitar. Meu palpite é que ele parou de chorar e decidiu fazer alguma coisa a respeito.</p>
+    </div>
+  )
 }
 
 export default HomePage
